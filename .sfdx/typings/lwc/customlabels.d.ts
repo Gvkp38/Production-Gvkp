ac7edb0e5d5343212aeedf612b0b021e743ad0f8
Vasanth <gvkp38@gmail.com>
2025-11-12 17:34:22 +0530

    commit all

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,16 @@
+declare module "@salesforce/label/c.calcyhelptext" {
+    var calcyhelptext: string;
+    export default calcyhelptext;
+}
+declare module "@salesforce/label/c.labeltest" {
+    var labeltest: string;
+    export default labeltest;
+}
+declare module "@salesforce/label/c.labeltest1" {
+    var labeltest1: string;
+    export default labeltest1;
+}
+declare module "@salesforce/label/c.welcome_message" {
+    var welcome_message: string;
+    export default welcome_message;
+}
